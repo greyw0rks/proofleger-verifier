a3c94146ec64489831ee35daddc47c06df7e6674
greyw0rks <elchapidave@gmail.com>
2026-05-21 20:30:31 +0100

    Add query-may10: query may10

--- src/query-may10.js
+++ src/query-may10.js
@@ -1,3 +1,3 @@
-// query-may10.js -- may10
-export const config = { version: 'may10', module: 'query-may10' };
+// generated: may10  extra: query-may10
+export const config = { version: 'may10', module: 'query-may10', ts: 95544 };
 export function init() { return true; }

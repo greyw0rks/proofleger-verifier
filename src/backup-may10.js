50d8854e51951b16b52369f47537dff886ccc459
greyw0rks <elchapidave@gmail.com>
2026-05-21 20:30:31 +0100

    Add backup-may10: backup may10

--- src/backup-may10.js
+++ src/backup-may10.js
@@ -1,3 +1,3 @@
-// backup-may10.js -- may10
-export const config = { version: 'may10', module: 'backup-may10' };
+// generated: may10  extra: backup-may10
+export const config = { version: 'may10', module: 'backup-may10', ts: 747493 };
 export function init() { return true; }

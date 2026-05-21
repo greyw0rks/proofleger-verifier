fe72705614f49ec873de5f5549191320d5a943dd
greyw0rks <elchapidave@gmail.com>
2026-05-21 20:30:31 +0100

    Add migration-may10: migration may10

--- src/migration-may10.js
+++ src/migration-may10.js
@@ -1,3 +1,3 @@
-// migration-may10.js -- may10
-export const config = { version: 'may10', module: 'migration-may10' };
+// generated: may10  extra: migration-may10
+export const config = { version: 'may10', module: 'migration-may10', ts: 823759 };
 export function init() { return true; }
